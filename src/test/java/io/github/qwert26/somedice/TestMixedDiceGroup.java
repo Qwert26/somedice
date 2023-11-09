5cd6495bca52782649ea98bc8ada63f0db5226e7
Christian Schürhoff <qwert268862@gmail.com>
2023-11-09 19:41:27 +0100

    Update TestMixedDiceGroup.java

--- src/test/java/io/github/qwert26/somedice/TestMixedDiceGroup.java
+++ src/test/java/io/github/qwert26/somedice/TestMixedDiceGroup.java
@@ -3,6 +3,7 @@ package io.github.qwert26.somedice;
 import org.junit.jupiter.api.*;
 
 @Tag("integration")
+@Disabled("Uncertain on how to test it.")
 public abstract class TestMixedDiceGroup {
 	// TODO
 }
