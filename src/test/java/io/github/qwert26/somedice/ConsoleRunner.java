380555a88cc37091b048d8e58be787bf7689032d
Qwert26 <qwert268862@gmail.com>
2023-11-12 14:58:23 +0100

    Create ConsoleRunner.java
    First practical test.

---
+++ src/test/java/io/github/qwert26/somedice/ConsoleRunner.java
@@ -0,0 +1,21 @@
+package io.github.qwert26.somedice;
+
+import java.util.*;
+
+public class ConsoleRunner {
+	public static void main(String[] args) {
+		SingleDie D6 = new SingleDie(false, 6);
+		SingleDie D8 = new SingleDie(8, false);
+		HomogenousDiceGroup threeD6 = new HomogenousDiceGroup(3, D6);
+		MixedDiceGroup threeD6AndD8 = new MixedDiceGroup(threeD6, D8);
+		DiceDropper dropLowest = new DiceDropper(threeD6AndD8);
+		dropLowest.setDropLowest(1);
+		dropLowest.setDropHighest(0);
+		Compressor comp = new Compressor(dropLowest);
+		UnfairDie result = comp.toUnfairDie();
+		System.out.println("Result for (3d6+d8)dl1:");
+		for (Map.Entry<Integer, Long> valueCount : result.getData().entrySet()) {
+			System.out.println("\t" + valueCount.getKey() + ": " + valueCount.getValue());
+		}
+	}
+}
