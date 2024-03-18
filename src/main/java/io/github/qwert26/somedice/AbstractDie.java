da36b06d6f24b48f6707c3d86aceea31b247c69b
Qwert26 <qwert268862@gmail.com>
2024-03-18 16:29:52 +0100

    Update AbstractDie.java
    Reorded JavaDoc-Tags to be consistent with the new Eclipse-Documentation-View.

--- src/main/java/io/github/qwert26/somedice/AbstractDie.java
+++ src/main/java/io/github/qwert26/somedice/AbstractDie.java
@@ -4,12 +4,11 @@ package io.github.qwert26.somedice;
  * An abstract die, as the code makes certain assumptions about the internal
  * workings of its subclasses, it is sealed.
  * 
+ * @author <b>Qwert26</b>, Main-author
  * @see SingleDie
  * @see FudgeDie
  * @see UnfairDie
  * @see RangeDie
- * 
- * @author Qwert26
  */
 public abstract sealed class AbstractDie implements IDie permits SingleDie, FudgeDie, UnfairDie, RangeDie {
 	/**
