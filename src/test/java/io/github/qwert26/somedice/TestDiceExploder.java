963ae132903e6b85245742c496f3494aa53f7f50
Qwert26 <qwert268862@gmail.com>
2024-11-25 17:52:57 +0100

    Update TestDiceExploder.java
    Added tests for equals.

--- src/test/java/io/github/qwert26/somedice/TestDiceExploder.java
+++ src/test/java/io/github/qwert26/somedice/TestDiceExploder.java
@@ -62,4 +62,93 @@ public class TestDiceExploder {
 		assertThrows(IllegalArgumentException.class, () -> new DiceExploder(explodeOn, explosionDepth, source));
 		assertThrows(IllegalArgumentException.class, () -> new DiceExploder(explosionDepth, explodeOn, source));
 	}
+
+	/**
+	 * 
+	 */
+	@Test
+	void checkEqualsNull() {
+		final AbstractDie source = new SingleDie(12);
+		final IntPredicate explodeOn = x -> true;
+		final byte explosionDepth = 1;
+		DiceExploder underTest = new DiceExploder(source, explodeOn, explosionDepth);
+		assertFalse(underTest.equals(null));
+	}
+
+	/**
+	 * 
+	 */
+	@Test
+	void checkEqualsItself() {
+		final AbstractDie source = new SingleDie(12);
+		final IntPredicate explodeOn = x -> true;
+		final byte explosionDepth = 1;
+		DiceExploder underTest = new DiceExploder(source, explosionDepth, explodeOn);
+		assertTrue(underTest.equals(underTest));
+	}
+
+	/**
+	 * 
+	 */
+	@Test
+	void checkEqualsWrongClass() {
+		final AbstractDie source = new SingleDie(12);
+		final IntPredicate explodeOn = x -> true;
+		final byte explosionDepth = 1;
+		DiceExploder underTest = new DiceExploder(explosionDepth, source, explodeOn);
+		Object other = new HomogeneousDiceGroup(source, explosionDepth);
+		assertFalse(underTest.equals(other));
+	}
+
+	/**
+	 * 
+	 */
+	@Test
+	void checkEqualsUnequalPredicate() {
+		final AbstractDie source = new SingleDie(12);
+		final IntPredicate explodeOn = x -> true;
+		final byte explosionDepth = 1;
+		DiceExploder underTest = new DiceExploder(explosionDepth, explodeOn, source);
+		DiceExploder other = new DiceExploder(explosionDepth, x -> false, source);
+		assertFalse(underTest.equals(other));
+	}
+
+	/**
+	 * 
+	 */
+	@Test
+	void checkEqualsUnequalSource() {
+		final AbstractDie source = new SingleDie(12);
+		final IntPredicate explodeOn = x -> true;
+		final byte explosionDepth = 1;
+		DiceExploder underTest = new DiceExploder(explodeOn, explosionDepth, source);
+		DiceExploder other = new DiceExploder(explodeOn, explosionDepth, new SingleDie(20));
+		assertFalse(underTest.equals(other));
+	}
+
+	/**
+	 * 
+	 */
+	@Test
+	void checkEqualsUnequalDepth() {
+		final AbstractDie source = new SingleDie(12);
+		final IntPredicate explodeOn = x -> true;
+		final byte explosionDepth = 1;
+		DiceExploder underTest = new DiceExploder(explodeOn, source, explosionDepth);
+		DiceExploder other = new DiceExploder(explodeOn, source, (byte) (explosionDepth + 1));
+		assertFalse(underTest.equals(other));
+	}
+
+	/**
+	 * 
+	 */
+	@Test
+	void checkEqualsIdentical() {
+		final AbstractDie source = new SingleDie(12);
+		final IntPredicate explodeOn = x -> true;
+		DiceExploder underTest = new DiceExploder(explodeOn, source);
+		DiceExploder other = new DiceExploder(source, explodeOn);
+		assertTrue(underTest.equals(other));
+		assertEquals(underTest.hashCode(), other.hashCode());
+	}
 }
